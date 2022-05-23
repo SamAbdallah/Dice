c793ff7ac1f12c6b3587117b7bdb731ad1d333f7
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-23 10:28:07 +0300

    Define variables

--- Lib/index.js
+++ Lib/index.js
@@ -1,6 +1,27 @@
 window.onload=function () {
+
+    
     var dice1 = Math.floor(Math.random() * 6) + 1;
     var dice1 = Math.floor(Math.random() * 6) + 1;
-    var img1= <img src="i" alt="">
+    var img1= new Image(100,200)
+    img1.src="images/dice1.png"
+    var img2= new Image(100,200)
+    img2.src="images/dice2.png"
+    var img3= new Image(100,200)
+    img3.src="images/dice3.png"
+    var img4= new Image(100,200)
+    img4.src="images/dice4.png"
+    var img5= new Image(100,200)
+    img5.src="images/dice5.png"
+    var img6= new Image(100,200)
+    img6.src="images/dice6.png"
+
+
+
+
+
+
+
+
 
 }
