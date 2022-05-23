8c4ee0f4bee7a08c9bb1a052b284e6b11ba57731
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-23 10:58:25 +0300

    Fixing variables and adding the if conditions

--- Lib/index.js
+++ Lib/index.js
@@ -1,21 +1,68 @@
 window.onload=function () {
 
-    
     var dice1 = Math.floor(Math.random() * 6) + 1;
-    var dice1 = Math.floor(Math.random() * 6) + 1;
-    var img1= new Image(100,200)
-    img1.src="images/dice1.png"
-    var img2= new Image(100,200)
-    img2.src="images/dice2.png"
-    var img3= new Image(100,200)
-    img3.src="images/dice3.png"
-    var img4= new Image(100,200)
-    img4.src="images/dice4.png"
-    var img5= new Image(100,200)
-    img5.src="images/dice5.png"
-    var img6= new Image(100,200)
-    img6.src="images/dice6.png"
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+    var head=document.getElementById("head")
 
+if (dice1==1){
+    document.getElementById("img1").src="images/dice1.png";
+}
+else if (dice1==2){
+    document.getElementById("img1").src="images/dice2.png";
+}
+
+else if (dice1==3){
+    document.getElementById("img1").src="images/dice3.png";
+}
+
+else if (dice1==4){
+    document.getElementById("img1").src="images/dice4.png";
+}
+
+else if (dice1==5){
+    document.getElementById("img1").src="images/dice5.png";
+}
+
+else if (dice1==6){
+    document.getElementById("img1").src="images/dice6.png";
+}
+
+
+
+if (dice2==1){
+    document.getElementById("img2").src="images/dice1.png";
+}
+else if (dice2==2){
+    document.getElementById("img2").src="images/dice2.png";
+}
+
+else if (dice2==3){
+    document.getElementById("img2").src="images/dice3.png";
+}
+
+else if (dice2==4){
+    document.getElementById("img2").src="images/dice4.png";
+}
+
+else if (dice2==5){
+    document.getElementById("img2").src="images/dice5.png";
+}
+
+else if (dice2==6){
+    document.getElementById("img2").src="images/dice6.png";
+}
+
+
+if (dice1>dice2){
+
+    head.innerHTML="Player 1 wins"
+}
+else if(dice1<dice2) {
+     head.innerHTML="Player 2 wins"
+}
+else if(dice1==dice2){
+    head.innerHTML="Draw!"
+}
 
 
 
